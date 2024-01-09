f6ff14a38ae53e6f5b0b86b47cc87cbccd1b562d
Josef Shainskiy <josef2011@gmail.com>
2024-01-09 20:31:17 +0200

    updare test

--- src/app/components/data-display/data-display.component.spec.ts
+++ src/app/components/data-display/data-display.component.spec.ts
@@ -1,15 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DataDisplayComponent } from './data-display.component';
+import { DataService } from '../../services/data.service';
+import { ChangeDetectorRef } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { DataObject } from '../../model/data-object.model';
+
+class MockDataService {
+  // Mock data for testing
+  private mockData: DataObject[] = [{
+    id: '1',
+    color: 'Test Data',
+    float:5,
+    int:5,
+    child: {
+      id: '1',
+      color: 'Test Data'
+    }
+  }];
+
+  // Mock dataStream Observable
+  get dataStream(): Observable<DataObject[]> {
+    return of(this.mockData);
+  }
+}
 
 describe('DataDisplayComponent', () => {
   let component: DataDisplayComponent;
   let fixture: ComponentFixture<DataDisplayComponent>;
+  let mockDataService: MockDataService;
+
+  beforeEach(async () => {
+    // Provide the mock service for DataService
+    await TestBed.configureTestingModule({
+      declarations: [DataDisplayComponent],
+      providers: [
+        { provide: DataService, useClass: MockDataService },
+        ChangeDetectorRef
+      ]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [DataDisplayComponent]
-    });
     fixture = TestBed.createComponent(DataDisplayComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +49,44 @@ describe('DataDisplayComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+
+  it('should update dataStream with data from service', () => {
+    // Arrange
+    const mockData = [{
+      id: '1',
+      color: 'Test Data',
+      float:5,
+      int:5,
+      child: {
+        id: '1',
+        color: 'Test Data'
+      }
+    }];
+    mockDataService = TestBed.inject(DataService) as unknown as MockDataService;
+
+    // Act
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    // Assert
+    expect(component.dataStream).toEqual(mockData);
+  });
+  // Test if the component correctly updates dataStream with data from the service
+  it('should update dataStream with data from service', () => {
+    mockDataService = TestBed.inject(DataService) as unknown as MockDataService;
+    component.ngOnInit();
+    expect(component.dataStream).toEqual([{
+      id: '1',
+      color: 'Test Data',
+      float:5,
+      int:5,
+      child: {
+        id: '1',
+        color: 'Test Data'
+      }
+    }]);
+  });
+
 });
